7dfce0a5e320127737c4cb03837593dc36d27a57
Christian Guevara <chrisdb150@gmail.com>
2022-04-04 13:05:50 -0400

    http methods

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,18 @@
-import { Controller, Get, Query, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Delete,
+  Put,
+  Body,
+  HttpStatus,
+  HttpCode,
+  Res,
+} from '@nestjs/common';
+
+import { Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
@@ -6,13 +20,17 @@ export class ProductsController {
     console.log('ProductsController');
   }
 
-  @Get('')
+  @Get()
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return `productos ${limit} y ${offset} y ${brand}`;
+    return {
+      limit,
+      offset,
+      brand,
+    };
   }
 
   @Get('filter')
@@ -21,7 +39,38 @@ export class ProductsController {
   }
 
   @Get(':id')
-  getProduct(@Param('id') id: string) {
-    return `product ${id}`;
+  @HttpCode(HttpStatus.ACCEPTED)
+  getProduct(@Res() response: Response, @Param('id') id: string) {
+    /*  response.status(200).send({
+      message: `product ${id}`,
+    }); */
+
+    return {
+      message: `product ${id}`,
+    };
+  }
+
+  @Post()
+  createProduct(@Body() payload: any) {
+    return {
+      message: 'Product created',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  updateProduct(@Param('id') id: string, @Body() payload: any) {
+    return {
+      message: 'Product updated',
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return {
+      message: `product ${id} deleted`,
+    };
   }
 }
