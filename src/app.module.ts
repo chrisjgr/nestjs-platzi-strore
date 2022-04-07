dc830ee4f568a4843c55733667fef6e6411473f8
Christian Guevara <chrisdb150@gmail.com>
2022-04-07 19:10:59 -0400

    validation of envs variables in prod woth joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 
+import * as Joi from 'joi';
+import { firstValueFrom } from 'rxjs';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-import { firstValueFrom } from 'rxjs';
 import { DatabaseModule } from './database/database.module';
 
 import { enviroments } from './enviroments';
@@ -20,6 +22,11 @@ import config from './config';
       envFilePath: enviroments[process.env.NODE_ENV] || ['.env'], // archivos env
       load: [config], // archivos de configuracion
       isGlobal: true, // acceso de la configuracion global
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     UsersModule,
