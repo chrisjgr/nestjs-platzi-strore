8de8a46ec6f3793f6c76a338d44565d6499e4ae1
Christian Guevara <chrisdb150@gmail.com>
2022-04-07 10:41:50 -0400

    use provider useValue example

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  constructor(@Inject('API_KEY') private apikey: string) {}
 
-  
+  getHello(): string {
+    return `Hell World! ${this.apikey}`;
+  }
 }
